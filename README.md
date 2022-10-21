e58a663d7894ff1acd00a10d9519255a13613cce
liuwei <captain.130@sjtu.edu.cn>
2022-10-21 16:10:51 +0800

    update

--- README.md
+++ README.md
@@ -1,2 +1,82 @@
-# awesome-hpo
-hpo update
+# Awesome Hyperparameter Optimization
+We would like to maintain a list of resources about Hyperparameter Optimization. 
+
+We mark work contributed by [Thinklab](http://thinklab.sjtu.edu.cn) with ✨.
+
+*Maintained by members in SJTU-Thinklab: Haoran Liao, Wei Liu, Shuwan Feng and Junchi Yan.*
+
+We are looking for post-docs interested in machine learning especially for learning combinatorial solvers, dynamic graphs, and reinforcement learning. Please send your up-to-date resume via yanjunchi AT sjtu.edu.cn.
+
+## [Content](#content)
+
+<!-- <tr><td><a href="#survey-papers">1. Survey</a></td></tr>  -->
+<table>
+
+<tr>
+	<td><a href=#survey>1 Survey</a></td>
+</tr>
+<tr>
+	<td><a href=#classical-optimization-technique>2 Classical Optimization Technique</a></td>
+</tr>
+<tr>
+	<td>&emsp;<a href=#classical-optimization-techniquesurvey>2.1 Survey</a></td>
+</tr>
+<tr>
+	<td>&emsp;<a href=#classical-optimization-techniquebayesian-optimization>2.2 Bayesian Optimization</a></td>
+</tr>
+<tr>
+	<td><a href=#application>3 Application</a></td>
+</tr>
+</table>
+
+
+
+
+### [Survey/paper](#content)
+
+1. **Algorithms for hyper-parameter optimization** NeurIPS, 2011. [journal](https://proceedings.neurips.cc/paper/2011/file/86e8f7ab32cfd12577bc2619bc635690-Paper.pdf)
+
+    *Bergstra, James and Bardenet, R{\'e}mi and Bengio, Yoshua and K{\'e}gl, Bal{\'a}zs*
+
+2. **Hyperparameter optimization in?learning systems** Journal of Membrane Computing, 2019. [journal](https://link.springer.com/article/10.1007/s41965-019-00023-0)
+
+    *Andonie, R{\u{a}}zvan*
+
+3. **Hyper-parameter optimization: A review of algorithms and applications** Arxiv, 2020. [journal](https://arxiv.org/pdf/2003.05689.pdf)
+
+    *Yu, Tong and Zhu, Hong*
+
+4. **On hyperparameter optimization of machine learning algorithms: Theory and practice** Neurocomputing, 2020. [journal](https://arxiv.org/pdf/2007.15745.pdf)
+
+    *Yang, Li and Shami, Abdallah*
+
+5. **A survey on hyperparameters optimization algorithms of forecasting models in smart grid** Sustainable Cities and Society, 2020. [journal](https://www.researchgate.net/profile/Nadeem-Javaid/publication/341464056_A_Survey_on_Hyperparameters_Optimization_Algorithms_of_Forecasting_Models_in_Smart_Grid/links/5ec2f1e192851c11a873ffbf/A-Survey-on-Hyperparameters-Optimization-Algorithms-of-Forecasting-Models-in-Smart-Grid.pdf)
+
+    *Khalid, Rabiya and Javaid, Nadeem*
+
+6. **AutoML: A survey of the state-of-the-art** Knowledge-Based Systems, 2021. [journal](https://arxiv.org/pdf/1908.00709.pdf?arxiv.org)
+
+    *He, Xin and Zhao, Kaiyong and Chu, Xiaowen*
+
+7. **Hyperparameter optimization: Foundations, algorithms, best practices and open challenges** Arxiv, 2021. [journal](https://arxiv.org/pdf/2107.05847.pdf)
+
+    *Bischl, Bernd and Binder, Martin and Lang, Michel and Pielok, Tobias and Richter, Jakob and Coors, Stefan and Thomas, Janek and Ullmann, Theresa and Becker, Marc and Boulesteix, Anne-Laure and others*
+
+### [Classical Optimization Technique/Survey/paper](#content)
+
+1. **Algorithms for hyper-parameter optimization** NeurIPS, 2011. [journal](https://proceedings.neurips.cc/paper/2011/file/86e8f7ab32cfd12577bc2619bc635690-Paper.pdf)
+
+    *Bergstra, James and Bardenet, R{\'e}mi and Bengio, Yoshua and K{\'e}gl, Bal{\'a}zs*
+
+### [Classical Optimization Technique/Bayesian Optimization/paper](#content)
+
+1. **Algorithms for hyper-parameter optimization** NeurIPS, 2011. [journal](https://proceedings.neurips.cc/paper/2011/file/86e8f7ab32cfd12577bc2619bc635690-Paper.pdf)
+
+    *Bergstra, James and Bardenet, R{\'e}mi and Bengio, Yoshua and K{\'e}gl, Bal{\'a}zs*
+
+### [Application/paper](#content)
+
+1. **A survey on hyperparameters optimization algorithms of forecasting models in smart grid** Sustainable Cities and Society, 2020. [journal](https://www.researchgate.net/profile/Nadeem-Javaid/publication/341464056_A_Survey_on_Hyperparameters_Optimization_Algorithms_of_Forecasting_Models_in_Smart_Grid/links/5ec2f1e192851c11a873ffbf/A-Survey-on-Hyperparameters-Optimization-Algorithms-of-Forecasting-Models-in-Smart-Grid.pdf)
+
+    *Khalid, Rabiya and Javaid, Nadeem*
+
---
+++ src/generetor.py
@@ -0,0 +1,130 @@
+import csv
+import os
+import copy
+
+abbr = {'Graph Matching': 'GM', 'Travelling Salesman Problem': 'TSP', 'Vehicle Routing Problem': 'VRP',
+        'Job Shop Scheduling Problem': 'JSSP', 'Bin Packing Problem': 'BPP', 'Graph Edit Distance': 'GED',
+        'Maximal Common Subgraph': 'MCS', 'Maximal Independent Set': 'MIS', 'Boolean Satisfiability': 'SAT',
+        'Quadratic Assignment Problem': 'QAP',
+        'Hamiltonian Cycle Problem': 'HCP',
+        'Multiple Travelling Salesman Problem': 'mTSP',
+        'Electronic Design Automation': 'EDA',
+        'Orienteering Problem': 'OP'}
+
+
+def sort_by_time(elem):
+    return elem[3]
+
+def write_category(file, content, n, index, href):
+    num = 0
+    for name in content.keys():
+        num += 1
+        if name == 'paper':
+            num -= 1
+            continue
+        file.writelines('<tr>\n')
+        name_index = name.replace(" ", "-").replace("/","").lower()
+        if name in abbr:
+            file.writelines('\t<td>'+'&emsp;'*n+'<a href=#{}>'.format(href+name_index)+ index + str(num) 
+                            +' {} ({})</a></td>\n'.format(name, abbr[name]))
+        else:
+            file.writelines('\t<td>'+'&emsp;'*n+'<a href=#{}>'.format(href+name_index)+ index + str(num)
+                            +' {}</a></td>\n'.format(name))
+        file.writelines('</tr>\n')
+        write_category(file, content[name], n+1, index+str(num)+'.', href+name_index)
+
+def write_content(file, content, path):
+    for name in content.keys():
+        path1 = path + name
+        if name == 'paper':
+            file.writelines("### [{}](#content)".format(path1))
+            file.write('\n')
+            file.write('\n')
+            num = 0
+            for paper in content[name]:
+                paper = [p.strip() for p in paper]
+                num += 1
+                # "category", "title", "publisher", "year", "type", "link", "authors, *code"
+                if paper[7] == "":
+                    file.writelines(
+                        "{}. **{}** {}, {}. [{}]({})".format(num, paper[1], paper[2], paper[3], paper[4], paper[5]))
+                else:
+                    file.writelines(
+                        "{}. **{}** {}, {}. [{}]({}), [code]({})".format(num, paper[1], paper[2], paper[3], paper[4],
+                                                                        paper[5], paper[7]))
+                file.write('\n')
+                file.write('\n')
+                file.writelines("    *{}*".format(paper[6]))
+                file.write('\n')
+                file.write('\n')
+            continue
+        write_content(file, content[name], path1+'/')
+
+
+def csv2md(csvFile, mdFile, header):
+    csvFile = open(csvFile, "r", encoding='utf-8')
+    reader = csv.reader(csvFile)
+    raw_papers = []
+    papers = []
+    for item in reader:
+        if reader.line_num == 1:
+            continue
+        raw_papers.append(item)
+    csvFile.close()
+
+    classes = []
+    for paper in raw_papers:
+        if ";" in paper[0]:
+            paper_classes = paper[0].split(";")
+            paper_classes = [cls.strip() for cls in paper_classes]
+        else:
+            paper_classes = [paper[0].strip()]
+        for cls in paper_classes:
+            if cls not in classes:
+                classes.append(cls)
+
+    content = {}
+    for c in classes:
+        path = c.split('//')
+        if path[0] not in content.keys():
+            content[path[0]] = {}
+        pa_1 = content[path[0]]
+        for pa in path[1:]:
+            if pa not in pa_1.keys():
+                pa_1[pa] = {}
+            pa_1 = pa_1[pa]
+
+    for c in classes:
+        p = []
+        for paper in raw_papers:
+            if c in paper[0]:
+                new_paper = copy.deepcopy(paper)
+                new_paper[0] = c
+                p.append(new_paper)
+        p.sort(key=sort_by_time)
+        path = c.split('//')
+        pa_1 = content[path[0]]
+        for pa in path[1:]:
+            pa_1 = pa_1[pa]
+        pa_1['paper'] = p
+
+
+    command = "cp " + header + " " + mdFile
+    os.system(command)
+    with open(mdFile, "a", encoding='utf-8') as file:
+        # write category
+        write_category(file, content, 0, '', '')
+        file.writelines('</table>\n')
+
+        # write content
+        file.write('\n')
+        file.write('\n')
+        file.write('\n')
+        file.write('\n')
+
+        write_content(file, content, '')
+
+
+if __name__ == '__main__':
+    # md2csv("../README.md", "../data/papers.csv")
+    csv2md("../data/papers.csv", "../README.md", "../data/header.md")
